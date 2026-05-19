f217852cad9181a98ec6bfa9536e95b43d858099
Jeffrey Lullen <lullenj@gmail.com>
2026-05-19 08:32:53 +0800

    Initial Commit

---
+++ MyFit/MyFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitApp.swift
+//  MyFit
+//
+//  Created by jeffrey lullen on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
